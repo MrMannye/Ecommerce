74d943294fdf466f5f5417a8083baa75a0eff10b
Miguel Aguilera <miguel.aguilera.ipn@gmail.com>
2026-08-03 18:29:49 -0600

    feat: create Home component with catalog layout and responsive styles

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,14 @@
+import "./Home.css"
 
 function Home() {
     return (
-        <div>Home</div>
+        <div>
+            <section className="container catalog">
+                <div className={`catalog__grid`}>
+                    Productos
+                </div>
+            </section>
+        </div >
     )
 }
 
